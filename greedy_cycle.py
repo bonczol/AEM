e7d51f6b3edc92322a6fcea1ccaaa8ff929c0536
wkupaj <wkupaj@gmail.com>
2020-03-20 14:17:23 +0100

    greedy cycle v2.0

--- greedy_cycle.py
+++ greedy_cycle.py
@@ -1,4 +1,5 @@
 import numpy as np
+import utilites as ut
 
 
 def greedy_cycle(start_point, distances):  # dodawanie kolenych łuków do cyklu Haminga
@@ -22,7 +23,7 @@ def greedy_cycle(start_point, distances):  # dodawanie kolenych łuków do cyklu
     position = 0
     # Rozciąganie cyklu ( założyłem, że najkrótszy łuk jest do najbliżeszego wierzchołka)
     # jak na razie nearest_neighbor ma i tak mniejszą długość
-    for i in range(n-2):
+    for i in range(n - 2):
         # TODO poprawic wyszukiwanie najblizszego wierzchołka
         global_min = np.inf
         for counter, j in enumerate(graf_wyj):
@@ -34,20 +35,53 @@ def greedy_cycle(start_point, distances):  # dodawanie kolenych łuków do cyklu
         nearest_point = point
         allowed[nearest_point] = False
 
-        #TODO poprawic wstawianie
+        # TODO poprawic wstawianie
         if graf_wyj.__len__() > position + 1:
 
-            if distances[nearest_point, graf_wyj[position+1]] < distances[nearest_point, graf_wyj[position - 1]]:
-                graf_wyj.insert(position+1, point)
+            if distances[nearest_point, graf_wyj[position + 1]] < distances[nearest_point, graf_wyj[position - 1]]:
+                graf_wyj.insert(position + 1, point)
+                print(11)
             else:
                 graf_wyj.insert(position, point)
+                print(12)
 
         else:
             if distances[nearest_point, graf_wyj[-1]] < distances[nearest_point, graf_wyj[position - 1]]:
-                graf_wyj.insert(position+1, point)
+                graf_wyj.insert(position + 1, point)
+                print(21)
             else:
                 graf_wyj.insert(position, point)
-
+                print(22)
     print(graf_wyj)
 
     return graf_wyj
+
+
+def greedy_cycle_v2(start_point, distances):  # dodawanie kolenych łuków do cyklu Haminga
+    n_all = distances.shape[0]
+    n = int(np.ceil(n_all / 2))
+
+    points_to_check = [x for x in range(n_all)]
+    print(points_to_check)
+    print(start_point)
+    points_to_check.remove(start_point)
+
+    global_solution = [start_point]
+
+    for i in range(n - 1):
+        global_result = np.inf
+        local_solution = global_solution.copy()
+        for count, j in enumerate(global_solution):
+            for k in points_to_check:
+                temp_solution = global_solution.copy()
+                temp_solution.insert(count, k)
+                local_result = ut.evaluate(temp_solution, distances)
+                if local_result < global_result:
+                    last_point=k
+                    global_result = local_result
+                    local_solution = temp_solution.copy()
+        points_to_check.remove(last_point)
+        global_solution = local_solution.copy()
+    print(global_solution)
+
+    return global_solution
