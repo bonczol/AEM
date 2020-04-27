6ae9999c8f3580b2c24a0d710c4930bc222ef45a
wkupaj <wkupaj@gmail.com>
2020-04-27 07:18:31 +0200

    LA_LM clean up

--- chyba_better_LS.py
+++ chyba_better_LS.py
@@ -1,8 +1,6 @@
 import numpy as np
 import itertools
-import functools
 import sys
-import utilites as ut
 
 np.set_printoptions(threshold=sys.maxsize)
 
@@ -39,124 +37,77 @@ def calc_exchange_delta_succ(p, o, path, outside, distance):
 
 
 def exchange_vertices_succ(path, outside, vp, vo):
-    # print("before:", path)
-    # print("before:", outside)
     id_vp = np.where(path == vp)[0]
     id_vo = np.where(outside == vo)[0]
     path[id_vp], outside[id_vo] = outside[id_vo], path[id_vp]
     return path, outside
 
 
-def steepest_v2(distances, instance):
+def steepest_v2(distances):
     n_all = distances.shape[0]
     n = int(np.ceil(n_all / 2))
 
-    # Wygeneruj rozwiązanie x
     path, outside = random_path(n, n_all)
-    # path = np.array([10, 30, 40, 61, 80, 90])
-    # outside = np.array([15, 25, 35, 45, 55])
-    # n = len(path)
     swap_actions = swap_edges_actions(n)
     exchange_actions = exchange_vertices_actions(path.shape[0], outside.shape[0])
 
-    # Zainicjuj LM – listę ruchów przynoszących poprawę uporządkowaną od najlepszego do najgorszego
     LM = []
     for i, s in enumerate(swap_actions):
-        delta = calc_swap_edges_delta_succ((path[s[0] - 1], path[s[0]]), (path[s[1]], path[(s[1] + 1) % len(path)]),
-                                           distances)
+        delta = calc_swap_edges_delta_succ((path[s[0] - 1], path[s[0]]), (path[s[1]], path[(s[1] + 1) % len(path)]), distances)
         if delta > 0:
-            add_to_list(LM, ["s", delta, s, (path[s[0] - 1], path[s[0]]), (path[s[1]], path[(s[1] + 1) % len(path)]),
-                             "init"])
+            add_to_list(LM, ["s", delta, (path[s[0] - 1], path[s[0]]), (path[s[1]], path[(s[1] + 1) % len(path)])])
 
     for i, e in enumerate(exchange_actions):
         delta = calc_exchange_delta_succ(e[0], e[1], path, outside, distances)
         if delta > 0:
-            add_to_list(LM, ["e", delta, e, path[e[0]], outside[e[1]], "init"])
+            add_to_list(LM, ["e", delta, path[e[0]], outside[e[1]]])
 
-    # powtarzaj
     id = 0
-    debug = True
     while LM and id < len(LM):
         first = LM[id]
-        # print(len(LM))
-        # ut.print_plot(instance, 0, path, "LS")
 
         if first[0] == "s":
-            if first[3][0] in path and first[3][1] in path and first[4][0] in path and first[4][1] in path:
-                if np.where(path == first[3][0])[0] == np.where(path == first[3][1])[0] - 1 and \
-                        np.where(path == first[4][0])[0] == np.where(path == first[4][1])[0] - 1:
-                    # if debug: print("--s------")
-                    path = swap_edges_succ(path, first[3], first[4])
-                    # print(first[3], first[4])
-
-                    # # znajdz nowe ruchy
-                    # for i, x in enumerate(path):
-                    #     x_next = path[(i + 1) % len(path)]
-                    #     if x_next != first[3][0] and x != first[3][0] and x != first[4][1]:
-                    #         delta = calc_swap_edges_delta_succ((first[3][0], first[4][1]), (x, x_next), distances)
-                    #         if delta > 0:
-                    #             add_to_list(LM, ["s", delta, 0, (first[3][0], first[4][1]), (x, x_next), "s1111"])
-                    #
-                    #     elif x_next != first[4][0] and x != first[4][0] and x != first[3][1]:
-                    #         x_next = path[(i + 1) % len(path)]
-                    #         delta = calc_swap_edges_delta_succ((first[4][0], first[3][1]), (x, x_next), distances)
-                    #         if delta > 0:
-                    #             add_to_list(LM, ["s", delta, 0, (first[4][0], first[3][1]), (x, x_next), "s2222"])
-
+            if first[2][0] in path and first[2][1] in path and first[3][0] in path and first[3][1] in path:
+                if np.where(path == first[2][0])[0] == np.where(path == first[2][1])[0] - 1 and \
+                        np.where(path == first[3][0])[0] == np.where(path == first[3][1])[0] - 1:
+                    path = swap_edges_succ(path, first[2], first[3])
                     for i, x in enumerate(path):
-                        for j, y in enumerate(path):
+                        x_next = path[(i + 1) % len(path)]
+                        if x_next != first[2][0] and x != first[2][0] and x != first[3][1]:
+                            delta = calc_swap_edges_delta_succ((first[2][0], first[3][1]), (x, x_next), distances)
+                            if delta > 0:
+                                add_to_list(LM, ["s", delta, (first[2][0], first[3][1]), (x, x_next)])
+
+                        elif x_next != first[3][0] and x != first[3][0] and x != first[2][1]:
                             x_next = path[(i + 1) % len(path)]
-                            y_next = path[(j + 1) % len(path)]
-                            if x_next != y and x != y_next and x != y:
-                                delta = calc_swap_edges_delta_succ((y, y_next), (x, x_next), distances)
-                                if delta > 0:
-                                    add_to_list(LM, ["s", delta, 0, (y, y_next), (x, x_next), "s1111"])
-
-
+                            delta = calc_swap_edges_delta_succ((first[3][0], first[2][1]), (x, x_next), distances)
+                            if delta > 0:
+                                add_to_list(LM, ["s", delta, (first[3][0], first[2][1]), (x, x_next)])
                     LM.pop(id)
                     id = 0
-
-                    # if debug:
-                    #     print(first)
-                    #     print(len(LM), LM)
-                    #     print("path:", path)
-                    #     print("outside:", outside)
-                    # ut.print_plot(instance, 0, path, "LS")
-
                 else:
                     id += 1
             else:
                 LM.pop(id)
 
-
         elif first[0] == "e":
-            if first[3] in path and first[4] in outside:
-                # if debug: print("--e------")
-                path, outside = exchange_vertices_succ(path, outside, first[3], first[4])
+            if first[2] in path and first[3] in outside:
+                path, outside = exchange_vertices_succ(path, outside, first[2], first[3])
 
-                # print(first[3], first[4])
                 id = 0
-                # znajdz nowe ruchy
                 for i, x in enumerate(path):
                     x_next = path[(i + 1) % len(path)]
-                    if x_next != first[4] and x != first[4]:
-                        out_next = path[(np.where(path == first[4])[0][0]) % len(path)]
+                    if x_next != first[3] and x != first[3]:
+                        out_next = path[(np.where(path == first[3])[0][0]) % len(path)]
                         if out_next != x:
-                            delta = calc_swap_edges_delta_succ((first[4], out_next), (x, x_next), distances)
+                            delta = calc_swap_edges_delta_succ((first[3], out_next), (x, x_next), distances)
                             if delta > 0:
-                                add_to_list(LM, ["s", delta, 0, (first[4], out_next), (x, x_next), "ess"])
+                                add_to_list(LM, ["s", delta, (first[3], out_next), (x, x_next)])
                 for i, x in enumerate(outside):
-                    index = (np.where(path == first[4])[0][0]) % len(path)
+                    index = (np.where(path == first[3])[0][0]) % len(path)
                     delta = calc_exchange_delta_succ(index, i, path, outside, distances)
                     if delta > 0:
-                        add_to_list(LM, ["e", delta, 0, first[4], x])
-                # if debug:
-                #     print(first)
-                #     print(len(LM), LM)
-                #     print("path:", path)
-                #     print("outside:", outside)
-                #     ut.print_plot(instance, 0, path, "LS")
-
+                        add_to_list(LM, ["e", delta, first[3], x])
             LM.remove(first)
 
     return path
